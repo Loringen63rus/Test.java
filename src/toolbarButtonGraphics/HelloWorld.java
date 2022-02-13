ab65d66440012c5789fd136d101c4c00ad4f1f2c
Loringen63rus <loringen63rus@yandex.ru>
2022-02-13 15:20:16 +0400

    work with what

---
+++ src/toolbarButtonGraphics/HelloWorld.java
@@ -0,0 +1,16 @@
+package toolbarButtonGraphics;
+
+import javax.swing.*;
+
+public class HelloWorld extends JFrame {
+
+    public HelloWorld() {
+        setSize(200, 300);
+        setTitle("Hello World");
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        var myHello = new HelloWorld();
+    }
+}
